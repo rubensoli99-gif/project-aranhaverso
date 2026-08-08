8b82e4ab9af05b5c8e900abbdf9a9356f2c2a763
rubens oliveira <ovitorrubens@gmail.com>
2026-08-08 16:15:08 -0300

    feat:estilizar e adicionar novos arquivos a pagina web

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const botao = document.querySelector("#entrar");
+const teia = document.querySelector("#teia");
+
+botao.addEventListener("click", function () {
+
+    // Ativa a animação da teia
+    teia.classList.add("ativa");
+
+    // Depois da animação, vai para a página inicial
+    setTimeout(function () {
+
+        window.location.href = "index.html";
+
+    }, 1500);
+
+});
+
